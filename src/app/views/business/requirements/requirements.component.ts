089b953d426778c5315a0be9f32cb229adc5856c
Rsmile310 <rsmile310@gmail.com>
2021-07-29 01:59:00 +1000

    Updated requirements.

--- src/app/views/business/requirements/requirements.component.ts
+++ src/app/views/business/requirements/requirements.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog, MatSnackBar, MatDialogRef } from '@angular/material';
+import { roles } from 'app/models/auth/roles';
 import { CampoBusca } from 'app/models/base/negocio/CampoBusca';
+import { AuthGuard } from 'app/services/auth/auth.guard';
 import { AppLoaderService } from 'app/services/dialogs/app-loader/app-loader.service';
 import { CRUDService } from 'app/services/negocio/CRUDService/CRUDService';
 import { DocumentsFormComponent } from '../documents/documents-form/documents-form.component';
@@ -18,23 +20,27 @@ export class RequirementsComponent implements OnInit {
   configSearch: any = [];
 
   columns = [
-    { Propriedade: 'area_name', Titulo: 'Sis.Gestão', Visivel: true, Largura:100 },
-    { Propriedade: 'area_aspect_name', Titulo: 'Aspecto', Visivel: true, Largura:100 },
+    { Propriedade: 'area_name', Titulo: 'Sis.Gestão', Visivel: true, Largura:200 },
+    { Propriedade: 'area_aspect_name', Titulo: 'Aspecto', Visivel: true, Largura:150 },
     { Propriedade: 'area_name', Titulo: 'Area', Visivel: true, Largura:100 },
     { Propriedade: 'document_item_subject', Titulo: 'Assunto', Visivel: true, Largura:100 },
     { Propriedade: 'document_item_subject', Titulo: 'Âmbito', Visivel: true, Largura:100 },
     { Propriedade: 'document_item_subject', Titulo: 'Documento', Visivel: true, Largura:100 },
     { Propriedade: 'document_item_subject', Titulo: 'Item', Visivel: true, Largura:100 },
     { Propriedade: 'document_item_subject', Titulo: 'Data/Status', Visivel: true, Largura:100 },
-    // { Propriedade: 'customer_business_name', Titulo: 'Cliente', Visivel: true, Largura: 300 },
-    // { Propriedade: 'customer_unity_name', Titulo: 'Unidade', Visivel: true, Largura: 300 },
+    // { Propriedade: 'customer_business_name', Titulo: 'Cliente', Visivel: true, Largura: 150 },
+    // { Propriedade: 'customer_unity_name', Titulo: 'Unidade', Visivel: true, Largura: 150 },
   ]
+
+  currentUser: any;
+  roles = roles;
 
   constructor(
     private crud: CRUDService,
     public dialog: MatDialog,
     private snackBar: MatSnackBar,
     private loader: AppLoaderService,
+    private auth: AuthGuard,
   ) { }
 
   prepareScreen() {
@@ -89,7 +95,7 @@ export class RequirementsComponent implements OnInit {
   }
 
   getDocuments(parameter: any) {
-    this.crud.GetParams(undefined, "/requirements").subscribe(res => {
+    this.crud.GetParams(undefined, `/requirements/${this.currentUser.role !== roles.admin ? (this.currentUser.customer_id || 0) : 'all'}`).subscribe(res => {
       this.rows = [];
       this.rows = res.body;
     })
@@ -100,6 +106,7 @@ export class RequirementsComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.currentUser = this.auth.getUser();
     this.prepareScreen();
   }
 
