9662c183001b0028577a2d0cdf41249a1e75cdbf
Rsmile310 <rsmile310@gmail.com>
2021-08-15 12:26:15 -0300

    Fixed bugs when add new documents

--- src/app/views/business/documents/documents-form/documents-form.component.ts
+++ src/app/views/business/documents/documents-form/documents-form.component.ts
@@ -62,7 +62,7 @@ export class DocumentsFormComponent implements OnInit {
       document_scope_id: new FormControl(record.document_scope_id, [Validators.required]),
       document_type: new FormControl(record.document_type, [Validators.required]),
       document_number: new FormControl(record.document_number, [Validators.required]),
-      document_date: new FormControl("", [Validators.required]),
+      document_date: new FormControl(record.document_date, [Validators.required]),
       document_status_id: new FormControl(record.document_status_id, [Validators.required]),
       document_summary: new FormControl(record.document_summary, [Validators.required]),
       document_state_id: new FormControl(record.document_state_id),
@@ -187,7 +187,7 @@ export class DocumentsFormComponent implements OnInit {
 
   saveDocument() {
     let form = this.documentForm.value;
-    form.document_date = Date.parse(form.document_date.substr(4, 4) + "-" + form.document_date.substr(2, 2) + "-" + ((parseInt(form.document_date.substr(0, 2)) )));
+    form.document_date = Date.parse(form.document_date);
     this.loader.open();
     this.crudService.Save(form, this.data.new, "/document", form.document_id).subscribe(res => {
       if (res.status == 200) {
@@ -259,6 +259,7 @@ export class DocumentsFormComponent implements OnInit {
   }
 
   getAttachments(documentId) {
+    if (documentId)
     this.crudService.GetParams({ "orderby": "createdAt", "direction": "asc" }, "/document-attachment/attachments/" + documentId).subscribe(res => {
       if (res.status == 200) {
         this.documentAttachments = [];
